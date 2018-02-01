a5e7e90a046c9d20d588bf3dda06aa249600e55a
Chris Prohoda <cgprohoda@gmail.com>
2018-02-01 00:41:46 -0500

    Finish infrastructure for downloading gff3 files

--- data_processing/download_genomes.py
+++ data_processing/download_genomes.py
@@ -1,11 +1,12 @@
 from ftplib import FTP
 import argparse
 import os
+import posixpath
 
 
 def resolve_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--datafile', dest='datafile', default='genomon', help='datafile name to get from source, defaults to genomon')
+    parser.add_argument('--datafile', dest='datafile', default='Gnomon', help='datafile name to get from source, defaults to genomon')
     parser.add_argument('--write_folder', dest='write_folder', default='../genomes/', help='local folder to write files')
     args = parser.parse_args()
     return args
@@ -17,14 +18,26 @@ def nih_ftp():
     return ftp
 
 
-def nih_genomes_files(ftp):
-    ftp.cwd('/genomes')
-    return ftp.nlst()
+def nih_genomes_filenames(ftp, datafile):
+    genomes_filenames = []
+    folders = ftp.nlst('/genomes')
+    for folder in folders:
+        try: 
+            data_folder = posixpath.join('/genomes', folder, datafile)
+            filenames = ftp.nlst(data_folder)
+            for filename in filenames:
+                if '_top_level.gff3.gz' in filename:
+                    abs_filename = posixpath.join(data_folder, filename)
+                    genomes_filenames.append(abs_filename)
+        except Exception as e:
+            print(type(e), e)
+            continue
+    return genomes_filenames
 
 
 def get_files_from_cwd(ftp, remote_filenames, local_folder):
     for filename in remote_filenames:
-        write_file = os.path.join(local_folder, filename)
+        write_file = os.path.join(local_folder, posixpath.basename(filename))
         with open(write_file,'w') as f:
             ftp.retrbinary('RETR '+filename, f.write)
 
@@ -32,6 +45,6 @@ def get_files_from_cwd(ftp, remote_filenames, local_folder):
 if __name__ == '__main__':
     args = resolve_args()
     ftp = nih_ftp()
-    filenames = nih_genomes_files(ftp)
+    filenames = nih_genomes_filenames(ftp, args.datafile)
     get_files_from_cwd(ftp=ftp, remote_filenames=filenames, local_folder=args.write_folder)
 
