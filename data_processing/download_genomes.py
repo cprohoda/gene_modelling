fa3fb41ea3375fa5c8b4fdc98ddf1d7512beb338
Chris Prohoda <cgprohoda@gmail.com>
2018-01-31 23:14:03 -0500

    Formatting cleanup

--- data_processing/download_genomes.py
+++ data_processing/download_genomes.py
@@ -3,24 +3,24 @@ import argparse
 
 
 def resolve_args():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--datafile', dest='datafile', default='genomon', help='datafile name to get from source, defaults to genomon')
-	parser.add_argument('--write_folder', dest='write_folder', default='../genomes/', help='local folder to write files')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--datafile', dest='datafile', default='genomon', help='datafile name to get from source, defaults to genomon')
+    parser.add_argument('--write_folder', dest='write_folder', default='../genomes/', help='local folder to write files')
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	return args
+    return args
 
-
+    
 def nih_genomes(filetype):
-	ftp = FTP('ftp.ncbi.nlm.nih.gov')
-	ftp.login()
-	ftp.cwd('genomes')
-	lines = ftp.retrlines('LIST')
-	print(type(lines))
-	print(lines)
+    ftp = FTP('ftp.ncbi.nlm.nih.gov')
+    ftp.login()
+    ftp.cwd('genomes')
+    lines = ftp.retrlines('LIST')
+    print(type(lines))
+    print(lines)
 
 
 if __name__ == '__main__':
-	args = resolve_args()
-	nih_genomes(args.datafile)
+    args = resolve_args()
+    nih_genomes(args.datafile)
