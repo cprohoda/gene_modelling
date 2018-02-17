ccf98e08f721b40e74fb8772a8097890198cf9d4
Chris Prohoda <cgprohoda@gmail.com>
2018-02-17 00:06:39 -0500

    Performance improvements to the gene mapping algorithm & data structure

--- data_processing/generate_gene_map.py
+++ data_processing/generate_gene_map.py
@@ -9,12 +9,13 @@ class GeneMap:
     def __init__(self, args):
         self.gene_map_filename = args.gene_map_filename
         self.processed_folder = args.processed_folder
-        self.genes_df = init_gene_map(args)
+        self.genes_df = self.init_gene_map(args)
+        self.gene_index_map = {}
 
-    def init_gene_map(args):
+    def init_gene_map(self, args):
         if args.overwrite or not os.path.isfile(args.gene_map_filename):
             processed_filenames = os.listdir(args.processed_folder)
-            genes_df = pd.DataFrame()
+            genes_df = pd.DataFrame(columns=processed_filenames, index=range(50000))
         else:
             genes_df = pd.read_csv(args.gene_map_filename, index_col=0)
         return genes_df
@@ -25,11 +26,20 @@ class GeneMap:
                 basename = os.path.basename(file.name)
                 print("Building gene map from file: {}".format(file.name))
                 for line in file:
-                    gene_number = extract_gene_number(line)
-                    gene_map.loc[gene_number, basename] = 1
+                    gene_index = self.map_gene_index(extract_gene_number(line))
+
+                    self.genes_df.loc[gene_index, basename] = 1
+                print("Memory usage: {}".format(self.genes_df.memory_usage(index=True).sum()))
         except Exception as e:
             print('Error generating gene map to {}\nError: {} {}'.format(self.gene_map_filename, type(e), e))
             raise
+
+    def map_gene_index(self, gene_number):
+        if gene_number in self.gene_index_map:
+            return self.gene_index_map[gene_number]
+        else:
+            self.gene_index_map[gene_number] = len(self.gene_index_map)
+            return self.gene_index_map[gene_number]
 
     def write(self, args):
         self.genes_df.to_csv(self.gene_map_filename)
@@ -46,9 +56,9 @@ def extract_gene_number(line):
 
 
 def get_processed_files(processed_folder):
-    processed_filenames = os.listdir(args.processed_folder)
+    processed_filenames = os.listdir(processed_folder)
     for processed_filename in processed_filenames:
-        processed_abs_filename = os.path.join(args.processed_folder, processed_filename)
+        processed_abs_filename = os.path.join(processed_folder, processed_filename)
         with open(processed_abs_filename, 'r') as f:
             yield f
 
