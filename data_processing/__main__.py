34f5a2dba2d13cdffc774198c0c6a37e9347951b
Chris Prohoda <cgprohoda@gmail.com>
2018-02-24 20:26:58 -0500

    Allow module __main__ to run scripts individually

--- data_processing/__main__.py
+++ data_processing/__main__.py
@@ -1,8 +1,13 @@
 import download_genomes
 import process_Gnomon
 import generate_gene_map
+from gene_modelling_utils import resolve_args
 
 if __name__=='__main__':
-    download_genomes.main()
-    process_Gnomon.main()
-    generate_gene_map.main()
+    args = resolve_args()
+    if 'download_genomes' in args.scripts:
+        download_genomes.main()
+    if 'process_Gnomon' in args.scripts:
+        process_Gnomon.main()
+    if 'generate_gene_map' in args.scripts:
+        generate_gene_map.main()
